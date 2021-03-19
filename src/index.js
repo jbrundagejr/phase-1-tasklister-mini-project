3356dee9461836046e975fd9535a279b9feceb58
Jon <jbrundagejr@gmail.com>
2021-03-19 14:59:07 -0400

    button added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let createTaskForm = document.querySelector("#create-task-form")
+let taskList = document.querySelector("#tasks")
+createTaskForm.addEventListener("submit", (evt) => {
+  evt.preventDefault()
+  let newTask = evt.target["new-task-description"].value
+  let newLi = document.createElement("li")
+  let newButton = document.createElement("button")
+  newButton.innerText = "X"
+  newButton.addEventListener("click", () => {
+    newLi.remove()
+  })
+  newLi.innerText = newTask
+  taskList.append(newLi)
+  newLi.append(newButton)
 });
